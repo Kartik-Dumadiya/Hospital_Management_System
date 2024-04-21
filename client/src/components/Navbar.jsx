cd1d63b638185204944f8b4c6553b13145a465f3
kartikdumadiya01 <kartikdumadiya0111@gmail.com>
2024-04-22 00:23:37 +0530

    changes hua he bhai

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import { Link , useNavigate, useLocation} from 'react-router-dom';
 import { useDispatch, useSelector} from 'react-redux';
 import { adminMenu, userMenu } from '../Data/Data';
 import { message, Badge} from 'antd';
+import noti from '../../media/noti_icon.webp';
+import logo from '../../media/logo.webp'
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -39,7 +41,7 @@ const Navbar = () => {
   return (
     <div className=' w-full bg-[#28328C] h-16 flex items-center justify-between'>
         <Link to="\" className=' cursor-pointer bg-[#28328C] h-full flex justify-start relative w-[200px]'>
-          <img src="..\media\logo.png" alt="Logo" className=' h-full'/>
+          <img src={logo} alt="Logo" className=' h-full'/>
           <p className=' fondamento-regular text-white text-3xl font-bold absolute bottom-2 right-5'>.Care</p>
         </Link>
 
@@ -55,10 +57,10 @@ const Navbar = () => {
         </div>
         { user ? <div className='flex gap-3 mr-3'>
                     <Badge count={user && user.notification.length} onClick={()=>{ navigate('/notification')}} className='cursor-pointer flex items-center bg-yellow-400 rounded-lg p-1 hover:bg-yellow-500 duration-200 ease-in py-2 px-3 gap-4 font-bold'>
-                      <img src="././media/noti_icon.png" alt="N" className='h-[20px] w-[20px]'/>
+                      <img src={noti} alt="N" className='h-[20px] w-[20px]'/>
                     </Badge>
                     <Link to="/login" onClick={handleLogout} className='cursor-pointer flex items-center bg-orange-400 rounded-lg p-1 hover:bg-red-500 duration-200 ease-in py-2 px-3 gap-4 font-bold'>
-                        <img src="../../media/logout_.png" className='h-[20px] w-[20px]' />
+                        <img src={"../../media/logout_.png"} className='h-[20px] w-[20px]' />
                         <p>Logout</p>
                     </Link>
                     <div className='cursor-pointer flex items-center bg-orange-400 rounded-lg p-1 hover:bg-orange-100 duration-200 ease-in py-2 px-3 gap-4 font-bold'>
